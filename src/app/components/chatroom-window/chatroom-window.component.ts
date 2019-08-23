f31d70c3c8b33a95a055f02c17b76773e1dc544e
Djan K. Blom <djan@live.dk>
2019-08-23 16:07:23 +0200

    more components added for chat layout

--- src/app/components/chatroom-window/chatroom-window.component.ts
+++ src/app/components/chatroom-window/chatroom-window.component.ts
@@ -6,6 +6,44 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./chatroom-window.component.scss']
 })
 export class ChatroomWindowComponent implements OnInit {
+  // Replace with firebase data :3
+  public dummyData = [
+    {
+      message: 'test',
+      createdAt: new Date(),
+      sender: {
+        firstName: 'Sally',
+        lastName: 'Jones',
+        photoUrl: 'http://via.placeholder.com/150x150'
+      }
+    },
+    {
+      message: 'asdasdasdfasfasdasd',
+      createdAt: new Date(),
+      sender: {
+        firstName: 'Kate',
+        lastName: 'Moss',
+        photoUrl: 'http://via.placeholder.com/150x150'
+      }
+    }, {
+      message: 'dlkfgjdjfgfsdsdf sdf dsfsd sfsdf',
+      createdAt: new Date(),
+      sender: {
+        firstName: 'Spider',
+        lastName: 'man',
+        photoUrl: 'http://via.placeholder.com/150x150'
+      }
+    },
+    {
+      message: 'Dooooooooooooom!',
+      createdAt: new Date(),
+      sender: {
+        firstName: 'Dr',
+        lastName: 'Doom',
+        photoUrl: 'http://via.placeholder.com/150x150'
+      }
+    },
+  ];
 
   constructor() { }
 
